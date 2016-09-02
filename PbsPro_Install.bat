726c20ad72ca46dd28d0814de90646ce688ae430
gajendrasharmagit <gajendrasharmagit@gmail.com>
2016-09-03 02:42:19 +0530

    PP-350: Create a basic PBS installer on Windows using Visual Studio 2008

--- PbsPro_Install.bat
+++ PbsPro_Install.bat
@@ -1,14 +1,15 @@
-xcopy "C:\Program Files\pgsql" "C:\Program Files (x86)\PBS Pro\exec\pgsql" /O /X /E /H /K /y
+xcopy "C:\Program Files\pgsql" "C:\Program Files (x86)\PBSPro\exec\pgsql" /O /X /E /H /K /y
 
 for /d /r "C:\Program Files\Tcl\bin" %%a in (*.dll) do (
-    copy "%%~fa" "C:\Program Files (x86)\PBS Pro\exec\bin"
+    copy "%%~fa" "C:\Program Files (x86)\PBSPro\exec\bin"
 )
 
-xcopy "c:\program files\Tcl\lib\tcl8.3" "C:\Program Files (x86)\PBS Pro\exec\lib\tcl8.3" /O /X /E /H /K /y
+xcopy "c:\program files\Tcl\lib\tcl8.3" "C:\Program Files (x86)\PBSPro\exec\lib\tcl8.3" /O /X /E /H /K /y
 
-xcopy "c:\program files\Tcl\lib\tk8.3" "C:\Program Files (x86)\PBS Pro\exec\lib\tk8.3" /O /X /E /H /K /y
+xcopy "c:\program files\Tcl\lib\tk8.3" "C:\Program Files (x86)\PBSPro\exec\lib\tk8.3" /O /X /E /H /K /y
 
-for /d /r "src\scheduler" %%a in (pbs_holidays*) do (
-    copy "%%~fa" "C:\Program Files (x86)\PBS Pro\exec\etc"
+for /d /r "C:\Program Files (x86)\PBSPro" %%a in (pbs_holidays*) do (
+    copy "%%~fa" "C:\Program Files (x86)\PBSPro\exec\etc"
 )
 
+copy "C:\Program Files (x86)\PBSPro\pbs_dataservice.bat" "C:\Program Files (x86)\PBS Pro\exec\bin"
